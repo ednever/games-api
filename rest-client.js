933637b1769ed68f406b2f15223be207921fc02b
ednever <edgarneverovskij@gmail.com>
2023-10-19 14:24:14 +0300

    Lisamine  WIP

--- rest-client.js
+++ rest-client.js
@@ -2,7 +2,8 @@ const vue = Vue.createApp({
     data() {
         return {
             gameInModal: {name: null},
-            games: []
+            games: [],
+            newGame: { id: null, name: null, price: null }
         }
     },
     async created() {
@@ -13,6 +14,31 @@ const vue = Vue.createApp({
             this.gameInModal = await (await fetch(`http://localhost:8080/games/${id}`)).json()
             let gameInfoModal = new bootstrap.Modal(document.getElementById('gameInfoModal'), {})
             gameInfoModal.show()
+        },
+        addGame: async function () {
+            // Подготовка данных для отправки на сервер
+            const data = {
+                id: this.newGame.id,
+                name: this.newGame.name,
+                price: this.newGame.price
+            };
+
+            // Отправка POST-запроса для добавления новой игры
+            const response = await fetch('http://localhost:8080/games/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok) {
+                // Если запрос успешен, обновляем список игр или выполняем другие действия
+                this.games = await response.json();
+            } else {
+                // Обработка ошибки, если не удалось добавить игру
+                console.error('Ошибка при добавлении игры');
+            }
         }
     }
 }).mount('#app')
